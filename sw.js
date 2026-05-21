65167384fb8b64acf7f6ec55d9ca52536aa9593a
msukri89 <mochammadsyukri@gmail.com>
2026-05-21 23:10:05 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const NAMA_CACHE = 'buku-pwa-v1';
+const fileYangDisimpan = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(NAMA_CACHE)
+            .then(cache => cache.addAll(fileYangDisimpan))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
