029bc3988aeccd6b7e9efcf83769712352f56d83
msukri89 <mochammadsyukri@gmail.com>
2026-05-22 00:32:22 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,16 @@
-const NAMA_CACHE = 'buku-pwa-v2';
-const fileYangDisimpan = [
-    './',
-    './index.html',
-    './style.css',
-    './app.js',
-    './manifest.json'
-];
+const NAMA_CACHE = 'buku-pwa-v5';
+const fileYangDisimpan = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(NAMA_CACHE)
-            .then(cache => cache.addAll(fileYangDisimpan))
-    );
+    self.skipWaiting(); // Paksa pakai versi baru
+    event.waitUntil(caches.open(NAMA_CACHE).then(cache => cache.addAll(fileYangDisimpan)));
+});
+
+self.addEventListener('activate', event => {
+    // Hapus memori nyangkut
+    event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== NAMA_CACHE).map(key => caches.delete(key)))));
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
